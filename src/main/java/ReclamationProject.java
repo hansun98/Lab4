24811b80f8910b9ab1abf29358b8fd443807d6c8
Sun Han <Sun Han@DESKTOP-0PKM8QQ>
2017-09-27 14:35:14 -0500

    Lab4 finish

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,51 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ * A class that Reclamation Project.
+ */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+public class ReclamationProject {
+    /**
+    * Complete the code for reclamation project.
+    * @param a1 a string
+    * @param b1 a string
+    * @return r string
+    */
+    static String doit(final String a1, final String b1) {
+        String a = a1;
+        String b = b1;
+        if (a.length() > b.length()) {
+            String c = a;
+            a = b;
+            b = c;
+            }
+        String r;
+        if (a.equals(b)) {
+            r = "";
+        } else {
+            r = "";
+        }
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+                 }
+                }
+        }
+        return r;
+        }
+
+    /**
+     * test doit.
+     * @param args argument
+     */
+    public static void main(final String[] args) {
+        System.out.print(doit("apple", "banana"));
+    }
 }
